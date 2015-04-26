ff02896f2a7918c4f3f0e47ae99085e8cd95ca97
Travis Kipp <TravisKippster@outlook.com>
2015-04-25 22:45:25 -0500

    Updated creator message
    Changed link to GitHub

--- scripts/namePrompt.js
+++ scripts/namePrompt.js
@@ -11,7 +11,7 @@ function namePrompt()
 		
 		if (name == "InvisaMage" || (name == "invisamage") || (name == "Travis") || (name == "travis"))
 		document.getElementById("name").innerHTML =
-		"Hello creator and welcome back to your website.<br>Here are some links I thought you might like.<br><br><span class='homeSection'><h3>Links</h3><a class=link title=Repository href=https://bitbucket.org/InvisaMage/website/>BitBucket</a></span>"
+		"Hello creator and welcome back to your website.<br>Here are some links I thought you might like.<br><br><span class='homeSection'><h3>Links</h3><a class=link title=Repository href=https://github.com/>GitHub</a></span>"
 		
 		if (name == "Matt" || (name == "matt") || (name == "Matthew") || (name == "matthew") || (name == "Tanner") || (name == "tanner") || (name == "Carson") || (name == "carson") || (name == "Chris") || (name == "chris"))
 		document.getElementById("name").innerHTML =
