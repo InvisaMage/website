60728c35d6b0475ef942acf4af7b511b5fb7db8a
Travis Kipp <TravisKippster@outlook.com>
2015-12-05 15:20:05 -0600

    Now works correctly.
    Plays until all assets are loaded.

--- js/preload.js
+++ js/preload.js
@@ -1,6 +1,10 @@
 // Fades out loading screen and fades in content.
 $(function() {
-  $('.preload').delay(300).fadeOut(300, function() {
+    $('.preload').delay(300);
+});
+
+$(window).load(function() {
+  $('.preload').fadeOut(300, function() {
     $('.content').fadeIn(300);
   });
 });
