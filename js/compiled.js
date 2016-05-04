6eb879af98dc1c68aef0a54840073afd781ec98a
Travis Kipp <TravisKippster@outlook.com>
2016-05-04 13:39:37 -0500

    Added more functions
    loadSnowstorm
    loadSnowstormNamePrompt
    loadWater
    destroyWater
    
    Tweaks to load terminal assets

--- js/compiled.js
+++ js/compiled.js
@@ -3,7 +3,6 @@
 //Preload - Fades out loading screen and fades in content.
 $(function() {
     NProgress.start();
-    $('.preload').delay(200);
 });
 
 $(window).load(function() {
@@ -69,19 +68,68 @@ $(function() {
   });
 });
 
+//Loads snowstorm assets and replaces footer text to enable snowstorm on next click
+function loadSnowstorm() {
+  $('#js-snowstorm').load('js/snowstorm.js');
+  $('#footer-snowstorm').tooltip('destroy');
+  $('#footer-snowstorm').replaceWith( "<a class='link' id='footer-snowstorm' onclick='snowStorm.toggleSnow();' data-toggle='tooltip' data-placement='top' title='Toggle the snowstorm!'>Toggle Snow</a>" );
+  //Re enable tooltips.
+  if(!('ontouchstart' in window))
+  {
+    $('[data-toggle="tooltip"]').tooltip();
+  }
+}
+
+//Loads snowstorm assets and enables for namePrompt easter egg
+function loadSnowstormNamePrompt(){
+  setTimeout(enable, 500);
+  $('#js-snowstorm').load('js/snowstorm.js');
+
+    function enable(){
+      snowStorm.toggleSnow();
+    }
+}
+
+//Loads water assets and replaces footer text to enable water on next click
+function loadWater() {
+  $('#js-water').load('js/jquery.ripples.js');
+  $('#footer-water').tooltip('destroy');
+  $('#footer-water').replaceWith( "<a class='link' id='footer-water' onclick='$(body).ripples({ resolution: 512, dropRadius: 20, perturbance: 0.04 }); destroyWater();' data-toggle='tooltip' data-placement='top' title='Enable a buggy water effect.'>Enable Water</a>" );
+  //Re enable tooltips.
+  if(!('ontouchstart' in window))
+  {
+    $('[data-toggle="tooltip"]').tooltip();
+  }
+}
+
+//Enables water and replaces footer text to enable water on next click
+function destroyWater() {
+  $('#footer-water').tooltip('destroy');
+  $('#footer-water').replaceWith( "<a class='link' id='footer-water' onclick='$(`body`).ripples(`destroy`); loadWater();' data-toggle='tooltip' data-placement='top' title='Mop up the water.'>Destroy Water</a>" );
+  //Re enable tooltips.
+  if(!('ontouchstart' in window))
+  {
+    $('[data-toggle="tooltip"]').tooltip();
+  }
+}
+
 //Loads footer on page load
 $('#footer').load('ajax/footer.html');
 
-//Loads terminal modal on page load
-$('#modal-terminal').load('ajax/modals/terminal.html');
+//Loads terminal modal on Shift + T key combination
+var listener = new window.keypress.Listener();
+listener.simple_combo("shift t", function() {
+    $('#js-ptty').load('js/ptty.jquery.min.js');
+    $('#modal-terminal').load('ajax/modals/terminal.html');
+});
 
 //Opens terminal with tilde key press
 var listener = new window.keypress.Listener();
 listener.simple_combo("`", function() {
     //Close open modals
-    $('#modal-personalize, #modal-hideads, #modal-contact, #modal-donate, #modal-stats, #modal-reload, #modal-eastereggs, #modal-search-help, #modal-yes-privacy, #modal-no-privacy, #modal-cookie, #modal-yes-terms, #modal-no-terms').modal('hide');
+    $('#modal-personalize, #modal-hideads, #modal-contact, #modal-donate, #modal-stats, #modal-reload, #modal-eastereggs, #modal-archive, #modal-search-help, #modal-privacy-yes, #modal-privacy-no, #modal-cookie, #modal-terms-yes, #modal-terms-no').modal('hide');
     $('#modal-terminal').modal('toggle');
-    $('#modal-terminal').on('shown.bs.modal', function (e) { $('input').focus(); });
+    $('#modal-terminal').on('shown.bs.modal', function (e) { $('form, input').focus(); });
 });
 
 console.log('What are you doing in here? \nYes I know I need to fix a few errors.')
