613f68cea37e7d076fc323681d8d027d3592562e
Travis Kipp <TravisKippster@outlook.com>
2016-08-09 12:39:30 -0500

    Functions for Options + many changes
    Moved checkAchievements(); to Achievements modal

--- js/compiled.js
+++ js/compiled.js
@@ -84,7 +84,6 @@ function loadSnowstorm() {
 function loadSnowstormNamePrompt(){
   setTimeout(enable, 500);
   $('#js-snowstorm').load('js/snowstorm.js');
-
     function enable(){
       snowStorm.toggleSnow();
     }
@@ -118,18 +117,25 @@ $('#footer').load('ajax/footer.html');
 //Loads modal references on page load
 $('#modals').load('ajax/modals.html');
 
-//Loads terminal modal on Shift + T key combination
-var listener = new window.keypress.Listener();
-listener.simple_combo("shift t", function() {
-    $('#js-ptty').load('js/ptty.min.js');
-    $('#modal-terminal').load('ajax/modals/terminal.html');
-});
-
 //Delete Easter egg on del key press
 var listener = new window.keypress.Listener();
 listener.simple_combo("delete", function() {
   var audio = new Audio('./audio/delete.mp3');
   audio.play();
+});
+
+//Loads terminal modal on Shift + T key combination
+var listener = new window.keypress.Listener();
+listener.simple_combo("shift t", function() {
+    $('#js-ptty').load('js/ptty.min.js');
+    $('#modal-terminal').load('ajax/modals/terminal.html');
+    $.bootstrapGrowl("Terminal assets have been loaded.<br><br>Press the <kbd>`</kbd> key to open.", {
+      type: 'success',
+      align: 'right',
+      offset: {from: 'top', amount: 60},
+      width: '300',
+      allow_dismiss: true
+    });
 });
 
 //Opens terminal with tilde key press
@@ -140,6 +146,120 @@ listener.simple_combo("`", function() {
     $('#modal-terminal').modal('toggle');
     $('#modal-terminal').on('shown.bs.modal', function (e) { $('form, input').focus(); });
     terminalCheck();
+});
+
+//Options modal logic (executed on save button click in Options modal)
+function optionsCheck() {
+  //Terminal
+  if ($('#terminal-radio1:checked').val() == 'yes') {
+    Cookies.set('loadTerminalOPL', 'true', { expires: 3600 });
+  }
+  if ($('#terminal-radio2:checked').val() == 'no') {
+    Cookies.set('loadTerminalOPL', 'false', { expires: 3600 });
+  }
+  //News
+  if ($('#news-radio1:checked').val() == 'projects') {
+    Cookies.set('defaultTab', 'projects', { expires: 3600 });
+  }
+  if ($('#news-radio2:checked').val() == 'important') {
+    Cookies.set('defaultTab', 'important', { expires: 3600 });
+  }
+  if ($('#news-radio3:checked').val() == 'website') {
+    Cookies.set('defaultTab', 'website', { expires: 3600 });
+  }
+  if ($('#news-radio4:checked').val() == 'bugs') {
+    Cookies.set('defaultTab', 'bugs', { expires: 3600 });
+  }
+  //Banners
+  if ($('#banners-radio1:checked').val() == 'yes') {
+    Cookies.set('loadBanners', 'true', { expires: 3600 });
+  }
+  if ($('#banners-radio2:checked').val() == 'no') {
+    Cookies.set('loadBanners', 'false', { expires: 3600 });
+  }
+  //Snowstorm
+  if ($('#snowstorm-radio1:checked').val() == 'yes') {
+    Cookies.set('enableSnowstorm', 'true', { expires: 3600 });
+  }
+  if ($('#snowstorm-radio2:checked').val() == 'no') {
+    Cookies.set('enableSnowstorm', 'false', { expires: 3600 });
+  }
+  $.bootstrapGrowl("Options Saved!", {
+    type: 'success',
+    align: 'right',
+    offset: {from: 'top', amount: 60},
+    width: 'auto',
+    delay: 10000,
+    allow_dismiss: true
+  });
+};
+
+//Options modal cancel message
+function optionsCancelMsg() {
+    $.bootstrapGrowl("Changes have not been saved!", {
+      type: 'danger',
+      align: 'right',
+      offset: {from: 'top', amount: 60},
+      width: 'auto',
+      delay: 10000,
+      allow_dismiss: true
+    });
+};
+
+//Check if Load Terminal on page load? is set to yes, if yes, load terminal assets.
+$(function() {
+  if (Cookies.get('loadTerminalOPL') == 'true') {
+    setTimeout(enable, 500);
+      function enable(){
+        $('#js-ptty').load('js/ptty.min.js');
+        $('#modal-terminal').load('ajax/modals/terminal.html');
+        console.log('Terminal assets have been loaded.\nUse tilde (`) key to open.');
+      }
+  } else {
+    console.log('loadTerminalOPL = false');
+  }
+});
+
+//Check which tab needs to be opened on News page.
+$(function() {
+  if (Cookies.get('defaultTab') == 'projects') {
+    $('#myTabs li:eq(0) a').tab('show');
+  }
+  else if (Cookies.get('defaultTab') == 'important') {
+    $('#myTabs li:eq(1) a').tab('show');
+  }
+  else if (Cookies.get('defaultTab') == 'website') {
+    $('#myTabs li:eq(2) a').tab('show');
+  }
+  else if (Cookies.get('defaultTab') == 'bugs') {
+    $('#myTabs li:eq(3) a').tab('show');
+  } else {
+    console.log('Error');
+  }
+});
+
+//Check if banners need to be displayed on T&C pages
+$(function() {
+  if (Cookies.get('loadBanners') == 'true') {
+    console.log('Banner has been loaded.')
+
+  } else {
+    console.log('loadBanners = false')
+    $('#alert').alert('close');
+  }
+});
+
+//Check if Snowstorm needs to be loaded on page load
+$(function() {
+  if (Cookies.get('enableSnowstorm') == 'true') {
+    setTimeout(enable, 500);
+    loadSnowstorm();
+      function enable(){
+        snowStorm.toggleSnow();
+      }
+  } else {
+    console.log('Snowstorm has not been loaded.')
+  }
 });
 
 //Contact modal copy messages
@@ -213,7 +333,7 @@ function eastereggCheck() {
       delay: 10000,
       allow_dismiss: true
     });
-    Cookies.set('eastereggAchievement', 'true', { expires: 30 });
+    Cookies.set('eastereggAchievement', 'true', { expires: 3600 });
   }
 }
 
@@ -230,7 +350,7 @@ function terminalCheck() {
       delay: 10000,
       allow_dismiss: true
     });
-    Cookies.set('terminalAchievement', 'true', { expires: 30 });
+    Cookies.set('terminalAchievement', 'true', { expires: 3600 });
   }
 }
 
@@ -239,7 +359,7 @@ function wiselyCheck() {
   if (Cookies.get('wisleyAchievement') == 'true') {
     console.log('Achievement message not displayed as user has already gotten it.')
   } else {
-    $.bootstrapGrowl("<strong>Achievement Get!</strong><br>Wisely<br><br>Agree to the <a href='terms.html'>Terms & Conditions</a> or <a href='privacy.html'>Privacy Policy</a>.", {
+    $.bootstrapGrowl("<strong>Achievement Get!</strong><br>You've Chosen Wisely<br><br>Agree to the <a href='terms.html'>Terms & Conditions</a> or <a href='privacy.html'>Privacy Policy</a>.", {
       type: 'info',
       align: 'right',
       offset: {from: 'top', amount: 60},
@@ -247,28 +367,7 @@ function wiselyCheck() {
       delay: 10000,
       allow_dismiss: true
     });
-    Cookies.set('wiselyAchievement', 'true', { expires: 30 });
-  }
-}
-
-//Check Achievements (used on modal).
-function checkAchievements() {
-  if (Cookies.get('eastereggAchievement') == 'true') {
-    $("#eastereggAchievement").replaceWith("<h5 class='text-success' id='eastereggAchievement'>Yes</h5>");
-  }else {
-    $("#eastereggAchievement").replaceWith("<h5 class='text-danger' id='eastereggAchievement'>No</h5>");
-  }
-
-  if (Cookies.get('terminalAchievement') == 'true') {
-    $("#terminalAchievement").replaceWith("<h5 class='text-success' id='terminalAchievement'>Yes</h5>");
-  }else {
-    $("#terminalAchievement").replaceWith("<h5 class='text-danger' id='terminalAchievement'>No</h5>");
-  }
-
-  if (Cookies.get('wiselyAchievement') == 'true') {
-    $("#wiselyAchievement").replaceWith("<h5 class='text-success' id='wiselyAchievement'>Yes</h5>");
-  }else {
-    $("#wiselyAchievement").replaceWith("<h5 class='text-danger' id='wiselyAchievement'>No</h5>");
+    Cookies.set('wiselyAchievement', 'true', { expires: 3600 });
   }
 }
 
@@ -277,6 +376,7 @@ function resetAchievements() {
   Cookies.remove('eastereggAchievement');
   Cookies.remove('terminalAchievement');
   Cookies.remove('wiselyAchievement');
+  Cookies.remove('hallucinatingAchievement');
   $('#btn-reset-achievements').tooltip('hide')
   $.bootstrapGrowl("Your achievement progress has been reset!", {
     type: 'info',
@@ -293,8 +393,22 @@ function loadAchievementsMod() {
   $('#modal-achievements').modal();
 }
 function loadAdsMod() {
-  $('#modal-hideads').load('ajax/modals/ads.html');
-  $('#modal-hideads').modal();
+  if (Cookies.get('hallucinatingAchievement') == 'true') {
+    $('#modal-hideads').load('ajax/modals/ads.html');
+    $('#modal-hideads').modal();
+  } else {
+    $('#modal-hideads').load('ajax/modals/ads.html');
+    $('#modal-hideads').modal();
+    Cookies.set('hallucinatingAchievement', 'true', { expires: 3600 });
+    $.bootstrapGrowl("<strong>Achievement Get!</strong><br>Hallucinating<br><br>Click Hide Ads in the footer.", {
+      type: 'info',
+      align: 'right',
+      offset: {from: 'top', amount: 60},
+      width: 'auto',
+      delay: 10000,
+      allow_dismiss: true
+    });
+  }
 }
 function loadArchiveMod() {
   $('#modal-archive').load('ajax/modals/archive.html');
@@ -315,6 +429,10 @@ function loadDonateMod() {
 function loadEastereggsMod() {
   $('#modal-eastereggs').load('ajax/modals/eastereggs.html');
   $('#modal-eastereggs').modal();
+}
+function loadOptionsMod() {
+  $('#modal-options').load('ajax/modals/options.html');
+  $('#modal-options').modal();
 }
 function loadPrivacyNoMod() {
   $('#modal-privacy-no').load('ajax/modals/privacy-no.html');
@@ -368,7 +486,6 @@ function pause() {
   $('audio').trigger('pause');
   $('#pause-btn').replaceWith( "<a id='play-btn' class='btn btn-success btn-md' role='button' onclick='play();'> <span class='glyphicon glyphicon-play' aria-hidden='true'></span> </a>" );
 }
-
 function play() {
   $('audio').trigger('play');
   $('#play-btn').replaceWith( "<a id='pause-btn' class='btn btn-success btn-md' role='button' onclick='pause();'> <span class='glyphicon glyphicon-pause' aria-hidden='true'></span> </a>" );
