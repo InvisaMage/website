7f447928782f8952bf34a0365473c70f8e6b7d61
Travis Kipp <TravisKippster@outlook.com>
2016-08-20 13:17:18 -0500

    Tweaks

--- js/search.js
+++ js/search.js
@@ -5,92 +5,134 @@ function search()
 	var search = document.getElementById("search").value || document.getElementById("search-mobile").value
 
 //Homepage
-	if (search == "Home" || (search == "home"))
+	if (search == "Home" || (search == "home")) {
 	window.location = "index.html";
+	}
 
 //News page
-	if (search == "News" || (search == "news"))
+	else if (search == "News" || (search == "news")) {
 	window.location = "news.html";
+	}
 
 //Floating Survival page
-	if (search == "Floating Survival" || (search == "floating survival") || (search == "FS") || (search == "fs") || (search == "Floating survival") || (search == "floating Survival"))
+	else if (search == "Floating Survival" || (search == "floating survival") || (search == "FS") || (search == "fs") || (search == "Floating survival") || (search == "floating Survival")) {
 	window.location = "fs.html";
+	}
 
 //InvisaRant page
-	if (search == "Podcast" || (search == "podcast") || (search == "InvisaRant") || (search == "invisarant") || (search == "Invisarant") || (search == "invisaRant"))
+	else if (search == "Podcast" || (search == "podcast") || (search == "InvisaRant") || (search == "invisarant") || (search == "Invisarant") || (search == "invisaRant")) {
 	window.location = "invisarant.html";
+	}
 
 //Contact Modal
-	if (search == "Contact" || (search == "contact") || (search == "Help") || (search == "help") || (search == "Support") || (search == "support") || (search == "Questions") || (search == "questions"))
+	else if (search == "Contact" || (search == "contact") || (search == "Help") || (search == "help") || (search == "Support") || (search == "support") || (search == "Questions") || (search == "questions")) {
 	loadContactMod();
+	}
 
 //Donate Modal
-	if (search == "Donate" || (search == "donate") || (search == "Money") || (search == "money"))
+	else if (search == "Donate" || (search == "donate") || (search == "Money") || (search == "money")) {
 	loadDonateMod();
+	}
 
 //Stats Modal
-	if (search == "Stats" || (search == "stats") || (search == "Statistics") || (search == "statistics") || (search == "Time") || (search == "time") || (search == "OS") || (search == "os") || (search == "Version") || (search == "version"))
+	else if (search == "Stats" || (search == "stats") || (search == "Statistics") || (search == "statistics") || (search == "Time") || (search == "time") || (search == "OS") || (search == "os") || (search == "Version") || (search == "version")) {
 	loadStatsMod();
+	}
 
 //About page
-	if (search == "About" || (search == "about") || (search == "InvisaMage") || (search == "invisamage") || (search == "Creation") || (search == "creation") || (search == "Information") || (search == "information"))
+	else if (search == "About" || (search == "about") || (search == "InvisaMage") || (search == "invisamage") || (search == "Creation") || (search == "creation") || (search == "Information") || (search == "information")) {
 	window.location = "about.html";
+	}
 
 //Terms page
-	if (search == "Terms" || (search == "terms") || (search == "Copyright") || (search == "copyright") || (search == "Legal") || (search == "legal") || (search == "Warranties") || (search == "warranties") || (search == "Liability") || (search == "liability"))
+	else if (search == "Terms" || (search == "terms") || (search == "Copyright") || (search == "copyright") || (search == "Legal") || (search == "legal") || (search == "Warranties") || (search == "warranties") || (search == "Liability") || (search == "liability")) {
 	window.location = "terms.html";
+	}
 
 //Privacy page
-	if (search == "Privacy" || (search == "privacy") || (search == "Cookies") || (search == "cookies"))
+	else if (search == "Privacy" || (search == "privacy") || (search == "Cookies") || (search == "cookies")) {
 	window.location = "privacy.html";
+	}
 
 //Archive modal
-	if (search == "Archive" || (search == "archive") || (search == "Old Pages") || (search == "old pages"))
+	else if (search == "Archive" || (search == "archive") || (search == "Old Pages") || (search == "old pages")) {
 	loadArchiveMod();
+	}
 
 //404 Error page
-	if (search == "404" || (search == "404 Error") || (search == "404 error") || (search == "Error") || (search == "error"))
+	else if (search == "404" || (search == "404 Error") || (search == "404 error") || (search == "Error") || (search == "error")) {
 	window.location = "404.html";
+	}
 
 //Terminal modal
-	if (search == "Terminal" || (search == "terminal") || (search == "Commands") || (search == "commands") || (search == "cmd"))
+	else if (search == "Terminal" || (search == "terminal") || (search == "Commands") || (search == "commands") || (search == "cmd")) {
 	loadTerminalMod();
+	}
+
+//Achievements modal
+	else if (search == "Achievements" || (search == "achievements") || (search == "Progress") || (search == "progress")) {
+	loadAchievementsMod();
+	}
+
+//Settings modal
+	else if (search == "Settings" || (search == "settings") || (search == "Options") || (search == "options") || (search == "Configure") || (search == "configure")) {
+	loadSettingsMod();
+	}
 
 //Google
-	if (search == "Google" || (search == "google"))
+	else if (search == "Google" || (search == "google")) {
 	window.open("https://www.google.com/");
+	}
 
 //Bing
-	if (search == "Bing" || (search == "bing"))
+	else if (search == "Bing" || (search == "bing")) {
 	window.open("https://www.bing.com/");
+	}
 
 //DuckDuckGo
-	if (search == "DuckDuckGo" || (search == "duckduckgo") || (search == "DuckGo") || (search == "duckgo") || (search == "Duck") || (search == "duck"))
+	else if (search == "DuckDuckGo" || (search == "duckduckgo") || (search == "DuckGo") || (search == "duckgo") || (search == "Duck") || (search == "duck")) {
 	window.open("https://duckduckgo.com/");
+	}
 
 //YouTube
-	if (search == "YouTube" || (search == "youtube") || (search == "YT") || (search == "yt"))
+	else if (search == "YouTube" || (search == "youtube") || (search == "YT") || (search == "yt")) {
 	window.open("https://www.youtube.com/");
+	}
 
 //RuneScape
-	if (search == "RuneScape" || (search == "runescape") || (search == "RS") || (search == "rs"))
+	else if (search == "RuneScape" || (search == "runescape") || (search == "RS") || (search == "rs")) {
 	window.location = ("rs-launch://www.runescape.com/jav_config.ws");
+	}
 
 //DarkScape
-	if (search == "DarkScape" || (search == "darkscape") || (search == "DS") || (search == "ds"))
+	else if (search == "DarkScape" || (search == "darkscape") || (search == "DS") || (search == "ds")) {
 	window.location = ("rs-launch://www.runescape.com/jav_config_beta.ws");
+	}
 
 //Cortana
-	if (search == "Cortana" || (search == "cortana"))
+	else if (search == "Cortana" || (search == "cortana")) {
 	window.location = ("ms-Cortana://");
+	}
 
 //Blank
-	if (search == "" || (search == " "))
-	$.bootstrapGrowl("There's nothing to search for!", {
-    type: 'danger',
-    align: 'right',
-    offset: {from: 'top', amount: 60},
-    width: 'auto',
-    allow_dismiss: true
-  });
+	else if (search == "" || (search == " ")) {
+		$.bootstrapGrowl("There's nothing to search for!", {
+	    type: 'danger',
+	    align: 'right',
+	    offset: {from: 'top', amount: 60},
+	    width: 'auto',
+	    allow_dismiss: true
+	  });
+	}
+
+//If no other conditions are true, display message.
+	else {
+		$.bootstrapGrowl("No Results", {
+	    type: 'danger',
+	    align: 'right',
+	    offset: {from: 'top', amount: 60},
+	    width: 'auto',
+	    allow_dismiss: true
+	  });
+	}
 }
