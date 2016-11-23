5ca3f36870d719e8a38126c51ae3db8e9e2bb6e9
Travis Kipp <TravisKippster@outlook.com>
2016-11-23 14:22:56 -0600

    Cookies are now sent on SSL only

--- js/compiled.js
+++ js/compiled.js
@@ -154,65 +154,65 @@ listener.simple_combo("`", function() {
 function settingsCheck() {
   //Theme
   if ($('#theme-radio1:checked').val() == 'dark') {
-    Cookies.set('theme', 'dark', {expires: 3600}, {secure: true});
+    Cookies.set('theme', 'dark', {expires: 3600, secure: true});
   }
   if ($('#theme-radio2:checked').val() == 'light') {
-    Cookies.set('theme', 'light', {expires: 3600}, {secure: true});
+    Cookies.set('theme', 'light', {expires: 3600, secure: true});
   }
   //Legacy background
   if ($('#legacy-background-checkbox1:checked').val() == 'true') {
-    Cookies.set('themeLegacyBg', 'true', {expires: 3600}, {secure: true});
+    Cookies.set('themeLegacyBg', 'true', {expires: 3600, secure: true});
   }
   if ($('#legacy-background-checkbox1:checked').val() != 'true') {
-    Cookies.set('themeLegacyBg', 'false', {expires: 3600}, {secure: true});
+    Cookies.set('themeLegacyBg', 'false', {expires: 3600, secure: true});
   }
   //Centered Modals
   if ($('#modal-centered-checkbox1:checked').val() == 'true') {
-    Cookies.set('centeredModals', 'true', {expires: 3600}, {secure: true});
+    Cookies.set('centeredModals', 'true', {expires: 3600, secure: true});
   }
   if ($('#modal-centered-checkbox1:checked').val() != 'true') {
-    Cookies.set('centeredModals', 'false', {expires: 3600}, {secure: true});
+    Cookies.set('centeredModals', 'false', {expires: 3600, secure: true});
   }
   //Banner - Homepage
   if ($('#home-banner-checkbox1:checked').val() == 'true') {
-    Cookies.set('enableEventsBanner', 'true', {expires: 3600}, {secure: true});
+    Cookies.set('enableEventsBanner', 'true', {expires: 3600, secure: true});
   }
   if ($('#home-banner-checkbox1:checked').val() != 'true') {
-    Cookies.set('enableEventsBanner', 'false', {expires: 3600}, {secure: true});
+    Cookies.set('enableEventsBanner', 'false', {expires: 3600, secure: true});
   }
   //Banner - Terms and conditions
   if ($('#tac-banner-checkbox1:checked').val() == 'true') {
-    Cookies.set('enableTacBanner', 'true', {expires: 3600}, {secure: true});
+    Cookies.set('enableTacBanner', 'true', {expires: 3600, secure: true});
   }
   if ($('#tac-banner-checkbox1:checked').val() != 'true') {
-    Cookies.set('enableTacBanner', 'false', {expires: 3600}, {secure: true});
+    Cookies.set('enableTacBanner', 'false', {expires: 3600, secure: true});
   }
   //News
   if ($('#news-radio1:checked').val() == 'projects') {
-    Cookies.set('defaultTab', 'projects', {expires: 3600}, {secure: true});
+    Cookies.set('defaultTab', 'projects', {expires: 3600, secure: true});
   }
   if ($('#news-radio2:checked').val() == 'important') {
-    Cookies.set('defaultTab', 'important', {expires: 3600}, {secure: true});
+    Cookies.set('defaultTab', 'important', {expires: 3600, secure: true});
   }
   if ($('#news-radio3:checked').val() == 'website') {
-    Cookies.set('defaultTab', 'website', {expires: 3600}, {secure: true});
+    Cookies.set('defaultTab', 'website', {expires: 3600, secure: true});
   }
   if ($('#news-radio4:checked').val() == 'bugs') {
-    Cookies.set('defaultTab', 'bugs', {expires: 3600}, {secure: true});
+    Cookies.set('defaultTab', 'bugs', {expires: 3600, secure: true});
   }
   //Terminal
   if ($('#terminal-checkbox1:checked').val() == 'true') {
-    Cookies.set('loadTerminal', 'true', {expires: 3600}, {secure: true});
+    Cookies.set('loadTerminal', 'true', {expires: 3600, secure: true});
   }
   if ($('#terminal-checkbox1:checked').val() != 'true') {
-    Cookies.set('loadTerminal', 'false', {expires: 3600}, {secure: true});
+    Cookies.set('loadTerminal', 'false', {expires: 3600, secure: true});
   }
   //Snowstorm
   if ($('#snowstorm-checkbox1:checked').val() == 'true') {
-    Cookies.set('enableSnowstorm', 'true', {expires: 3600}, {secure: true});
+    Cookies.set('enableSnowstorm', 'true', {expires: 3600, secure: true});
   }
   if ($('#snowstorm-checkbox1:checked').val() != 'true') {
-    Cookies.set('enableSnowstorm', 'false', {expires: 3600}, {secure: true});
+    Cookies.set('enableSnowstorm', 'false', {expires: 3600, secure: true});
   }
   setTimeout(enable, 1500);
   $.bootstrapGrowl("Settings Saved!", {
@@ -339,10 +339,10 @@ $(function() {
 //Forces to set banner cookies if they have not been set yet
 $(function() {
   if (Cookies.get('enableEventsBanner') == undefined) {
-      Cookies.set('enableEventsBanner', 'true', {expires: 3600}, {secure: true});
+      Cookies.set('enableEventsBanner', 'true', {expires: 3600, secure: true});
   }
   if (Cookies.get('enableTacBanner') == undefined) {
-      Cookies.set('enableTacBanner', 'true', {expires: 3600}, {secure: true});
+      Cookies.set('enableTacBanner', 'true', {expires: 3600, secure: true});
   }
 });
 
@@ -456,7 +456,7 @@ function eastereggCheck() {
       delay: 10000,
       allow_dismiss: true
     });
-    Cookies.set('eastereggAchievement', 'true', {expires: 3600}, {secure: true});
+    Cookies.set('eastereggAchievement', 'true', {expires: 3600, secure: true});
   }
 }
 
@@ -473,7 +473,7 @@ function terminalCheck() {
       delay: 10000,
       allow_dismiss: true
     });
-    Cookies.set('terminalAchievement', 'true', {expires: 3600}, {secure: true});
+    Cookies.set('terminalAchievement', 'true', {expires: 3600, secure: true});
   }
 }
 
@@ -490,7 +490,7 @@ function wiselyCheck() {
       delay: 10000,
       allow_dismiss: true
     });
-    Cookies.set('wiselyAchievement', 'true', {expires: 3600}, {secure: true});
+    Cookies.set('wiselyAchievement', 'true', {expires: 3600, secure: true});
   }
 }
 
@@ -507,7 +507,7 @@ function purplerainCheck() {
       delay: 10000,
       allow_dismiss: true
     });
-    Cookies.set('purplerainAchievement', 'true', {expires: 3600}, {secure: true});
+    Cookies.set('purplerainAchievement', 'true', {expires: 3600, secure: true});
   }
 }
 
@@ -541,7 +541,7 @@ function loadAdsMod() {
   } else {
     $('#modal-hideads').load('ajax/modals/ads.html');
     $('#modal-hideads').modal();
-    Cookies.set('hallucinatingAchievement', 'true', {expires: 3600}, {secure: true});
+    Cookies.set('hallucinatingAchievement', 'true', {expires: 3600, secure: true});
     $.bootstrapGrowl("<strong>Achievement Get!</strong><br>Hallucinating<br><br>Click Hide Ads in the footer.", {
       type: 'info',
       align: 'right',
