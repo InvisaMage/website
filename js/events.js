b2631e46e09588ecdcdf8341183f4b24885a644a
Travis Kipp <TravisKippster@outlook.com>
2017-06-14 01:29:29 -0500

    June 14, 2017
    Floating Survival images are once again hosted locally.
    The image sizes have been optimized for faster transfer rates.
    
    Added more animations to pages.
    The Settings Modal will now warn if the site is being viewed locally. (Not http(s)).
    JavaScript tweaks.

--- js/events.js
+++ js/events.js
@@ -13,51 +13,51 @@ $('#event-banner').toggle();
 var date = new Date;
 
 //New Year
-if (date.getMonth() == 0 && (date.getDate() == 1)) {
+if (date.getMonth() === 0 && (date.getDate() == 1)) {
   	document.getElementById("event-banner-content").innerHTML =
     "Happy New Year!<br>" +
-    "Did you make any New Year's resolutions?"
+    "Did you make any New Year's resolutions?";
     $('#event-banner').toggle();
 }
 //Star Wars Day
 if (date.getMonth() == 4 && (date.getDate() == 4)) {
   	document.getElementById("event-banner-content").innerHTML =
     "Happy Star Wars Day!<br>" +
-    "May the fourth be with you!"
+    "May the fourth be with you!";
     $('#event-banner').toggle();
 }
 //Independence Day
 if (date.getMonth() == 6 && (date.getDate() == 4)) {
   	document.getElementById("event-banner-content").innerHTML =
     "Happy Independence Day!<br>" +
-    "BBQ time!"
+    "BBQ time!";
     $('#event-banner').toggle();
 }
 //Owner's birthday
 if (date.getMonth() == 8 && (date.getDate() == 28)) {
   	document.getElementById("event-banner-content").innerHTML =
     "Today is the owner's birthday.<br>" +
-    "Happy Birthday Travis!"
+    "Happy Birthday Travis!";
     $('#event-banner').toggle();
 }
 //Halloween
 if (date.getMonth() == 9 && (date.getDate() == 31)) {
   	document.getElementById("event-banner-content").innerHTML =
     "Happy Holloween!<br>" +
-		"Are you going to get any candy?"
+		"Are you going to get any candy?";
     $('#event-banner').toggle();
 }
 //Christmas eve
 if (date.getMonth() == 11 && (date.getDate() == 24)) {
   	document.getElementById("event-banner-content").innerHTML =
     "Christmas Eve<br>" +
-		"'Tis the night before Christmas..."
+		"'Tis the night before Christmas...";
     $('#event-banner').toggle();
 }
 //Christmas
 if (date.getMonth() == 11 && (date.getDate() == 25)) {
   	document.getElementById("event-banner-content").innerHTML =
     "Merry Christmas!<br>" +
-		"<a onclick='loadSnowstormNamePrompt();'>Let it Snow!</a>"
+		"<a onclick='loadSnowstormNamePrompt();'>Let it Snow!</a>";
     $('#event-banner').toggle();
 }
